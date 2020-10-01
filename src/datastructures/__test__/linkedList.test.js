b189890c3cad4b86306e087f639c8d246f0ac3d4
Kiera Nguyen <ngannguyen9017@gmail.com>
2020-09-30 17:47:04 -0700

    Implemented and unit tested SinglyLinkedList

--- src/datastructures/__test__/linkedList.test.js
+++ src/datastructures/__test__/linkedList.test.js
@@ -1,185 +1,256 @@
-import { DoublyLinkedList } from '../linkedList';
+import { DoublyLinkedList, SinglyLinkedList } from '../linkedList';
 
-describe('Testing on DoublyLinkedList', () => {
-  let list;
+const linkedListTest = (List, listName) => {
+  describe(`Testing on ${listName}`, () => {
+    let list;
 
-  describe('Test methods on empty list', () => {
-    beforeEach(() => {
-      list = new DoublyLinkedList();
-    });
-
-    test('List is empty with size = 0', () => {
-      expect(list.size()).toBe(0);
-      expect(list.isEmpty()).toBe(true);
-    });
-
-    test('Calling peak first/last on empty list returns null', () => {
-      expect(list.peakFirst()).toBeNull();
-      expect(list.peakLast()).toBeNull();
-    });
-
-    test('Calling removing first/last on empty list returns undefined', () => {
-      expect(list.removeFirst()).toBeUndefined();
-      expect(list.removeLast()).toBeUndefined();
-    });
-
-    test('Calling clear() on an empty list should simply do nothing or return undefined', () => {
-      expect(list.clear()).toBeUndefined();
-    });
-  });
-
-  describe('Test adding methods on list', () => {
-    beforeEach(() => {
-      list = new DoublyLinkedList();
-      list.add(1);
-      expect(list.size()).toBe(1);
-      expect(list.toString()).toBe('1');
-    });
-
-    test('Calling addFirst puts the new element at the beginning of the list', () => {
-      list.addFirst(0);
-      expect(list.size()).toBe(2);
-      expect(list.toString()).toBe('0 1');
-
-      expect(() => list.insert(0)).not.toThrow('Duplicate values');
-    });
-
-    test('Calling addLast or add puts new element at the end of the list', () => {
-      list.addLast(2);
-      expect(list.size()).toBe(2);
-      expect(list.toString()).toBe('1 2');
-
-      list.add(3);
-      expect(list.size()).toBe(3);
-      expect(list.toString()).toBe('1 2 3');
-
-      expect(() => list.insert(2)).not.toThrow('Duplicate values');
-    });
-
-    test('Calling insert (with index) should put new element at that index, or throws an Error if index is out of bound', () => {
-      list.add(3);
-      expect(list.size()).toBe(2);
-      expect(list.toString()).toBe('1 3');
-
-      expect(() => list.insert(0, -1)).toThrow('Index out of range');
-      expect(() => list.insert(0, 4)).toThrow('Index out of range');
-
-      list.insert(2, 1);
-      expect(list.size()).toBe(3);
-      expect(list.toString()).toBe('1 2 3');
-
-      list.insert(0, 0);
-      expect(list.size()).toBe(4);
-      expect(list.toString()).toBe('0 1 2 3');
-
-      list.insert(4, 4);
-      expect(list.size()).toBe(5);
-      expect(list.toString()).toBe('0 1 2 3 4');
-
-      expect(() => list.insert(2)).not.toThrow('Duplicate values');
-    });
-  });
-
-  describe('Test other methods on non-empty list', () => {
-    beforeEach(() => {
-      list = new DoublyLinkedList();
-      list.add(1);
-      list.add(5);
-      list.add(9);
-      list.addLast(10);
-      list.addFirst(0);
-      list.insert(2, 2);
-      expect(list.size()).toBe(6);
-      expect(list.toString()).toBe('0 1 2 5 9 10');
-    });
-
-    test('Removing methods should remove the correct element from the list', () => {
-      let value = list.removeFirst();
-      expect(value).toBe(0);
-      expect(list.size()).toBe(5);
-      expect(list.toString()).toBe('1 2 5 9 10');
-
-      value = list.removeLast();
-      expect(value).toBe(10);
-      expect(list.size()).toBe(4);
-      expect(list.toString()).toBe('1 2 5 9');
-
-      expect(() => list.removeAt(-1)).toThrow('Index out of range');
-      expect(() => list.removeAt(4)).toThrow('Index out of range');
-
-      // remove at the first index
-      value = list.removeAt(0);
-      expect(value).toBe(1);
-      expect(list.size()).toBe(3);
-      expect(list.toString()).toBe('2 5 9');
-
-      // add 1 back
-      list.addFirst(1);
-      expect(list.size()).toBe(4);
-      expect(list.toString()).toBe('1 2 5 9');
-
-      // remove at the last index
-      value = list.removeAt(3);
-      expect(value).toBe(9);
-      expect(list.size()).toBe(3);
-      expect(list.toString()).toBe('1 2 5');
-
-      // add 9 back
-      list.addLast(9);
-      expect(list.size()).toBe(4);
-      expect(list.toString()).toBe('1 2 5 9');
-
-      // remove at an index in the middle
-      value = list.removeAt(1);
-      expect(value).toBe(2);
-      expect(list.size()).toBe(3);
-      expect(list.toString()).toBe('1 5 9');
-
-      value = list.removeNodeWithValue(10);
-      expect(value).toBe(10);
-      expect(list.size()).toBe(3);
-
-      value = list.removeNodeWithValue(9);
-      expect(value).toBe(9);
-      expect(list.size()).toBe(2);
-      expect(list.toString()).toBe('1 5');
-    });
-
-    test('Peaking first/last should return the appropriate value', () => {
-      expect(list.peakFirst()).toBe(0);
-      expect(list.peakLast()).toBe(10);
-
-      list.add(11);
-      expect(list.toString()).toBe('0 1 2 5 9 10 11');
-      expect(list.peakLast()).toBe(11);
-      list.addFirst(-1);
-      expect(list.toString()).toBe('-1 0 1 2 5 9 10 11');
-      expect(list.peakFirst()).toBe(-1);
-
-      list.removeFirst();
-      expect(list.toString()).toBe('0 1 2 5 9 10 11');
-      expect(list.peakFirst()).toBe(0);
-
-      list.removeLast();
-      expect(list.toString()).toBe('0 1 2 5 9 10');
-      expect(list.peakLast()).toBe(10);
-    });
-
-    test('Clearing the list should remove all elements from the list', () => {
+    afterEach(() => {
       list.clear();
-      expect(list.isEmpty()).toBeTruthy();
-      expect(list.size()).toBe(0);
+      list = undefined;
     });
 
-    test('IndexOf method should return the correct index of the value in the list, or -1 if it does not exist', () => {
-      expect(list.indexOf(3)).toBe(-1);
-      expect(list.indexOf(1)).toBe(1);
-      expect(list.indexOf(9)).toBe(4);
+    describe('Test methods on empty list', () => {
+      beforeEach(() => {
+        list = new List();
+      });
+
+      test('List is empty with size = 0', () => {
+        expect(list.size()).toBe(0);
+        expect(list.isEmpty()).toBe(true);
+      });
+
+      test('Calling peak first/last on empty list returns null', () => {
+        expect(list.peakFirst()).toBeNull();
+        expect(list.peakLast()).toBeNull();
+      });
+
+      test('Calling removing first/last on empty list returns undefined', () => {
+        expect(list.removeFirst()).toBeUndefined();
+        expect(list.removeLast()).toBeUndefined();
+      });
+
+      test('Calling clear() on an empty list should simply do nothing or return undefined', () => {
+        expect(list.clear()).toBeUndefined();
+      });
+
+      test('Calling removeAt on an empty list will result in an exception', () => {
+        expect(() => list.removeAt(-1)).toThrow('Index out of range');
+        expect(() => list.removeAt(0)).toThrow('Index out of range');
+        expect(() => list.removeAt(1)).toThrow('Index out of range');
+      });
+
+      test('Calling removeFirstNodeWithValue or removeAllNodesWithValue on an empty list will return false', () => {
+        expect(list.removeFirstNodeWithValue()).toBeFalsy();
+        expect(list.removeAllNodesWithValue()).toBeFalsy();
+      });
     });
 
-    test('Contains method should return true if the value exist, otherwise false', () => {
-      expect(list.contains(3)).toBeFalsy();
-      expect(list.contains(10)).toBeTruthy();
+    describe('Test adding methods on list', () => {
+      beforeEach(() => {
+        list = new List();
+        list.add(1);
+        expect(list.size()).toBe(1);
+        expect(list.toString()).toBe('1');
+      });
+
+      test('Calling addFirst puts the new element at the beginning of the list', () => {
+        list.addFirst(0);
+        expect(list.size()).toBe(2);
+        expect(list.toString()).toBe('0 1');
+
+        expect(() => list.insert(0)).not.toThrow('Duplicate values');
+      });
+
+      test('Calling addLast or add puts new element at the end of the list', () => {
+        list.addLast(2);
+        expect(list.size()).toBe(2);
+        expect(list.toString()).toBe('1 2');
+
+        list.add(3);
+        expect(list.size()).toBe(3);
+        expect(list.toString()).toBe('1 2 3');
+
+        expect(() => list.insert(2)).not.toThrow('Duplicate values');
+      });
+
+      test('Calling insert (with index) should put new element at that index, or throws an Error if index is out of bound', () => {
+        list.add(3);
+        expect(list.size()).toBe(2);
+        expect(list.toString()).toBe('1 3');
+
+        expect(() => list.insert(0, -1)).toThrow('Index out of range');
+        expect(() => list.insert(0, 4)).toThrow('Index out of range');
+
+        list.insert(2, 1);
+        expect(list.size()).toBe(3);
+        expect(list.toString()).toBe('1 2 3');
+
+        list.insert(0, 0);
+        expect(list.size()).toBe(4);
+        expect(list.toString()).toBe('0 1 2 3');
+
+        list.insert(4, 4);
+        expect(list.size()).toBe(5);
+        expect(list.toString()).toBe('0 1 2 3 4');
+
+        expect(() => list.insert(2)).not.toThrow('Duplicate values');
+      });
+    });
+
+    describe('Test other methods on non-empty list', () => {
+      beforeEach(() => {
+        list = new List();
+        list.add(1);
+        list.add(5);
+        list.add(9);
+        list.addLast(10);
+        list.addFirst(0);
+        list.insert(2, 2);
+        expect(list.size()).toBe(6);
+        expect(list.toString()).toBe('0 1 2 5 9 10');
+      });
+
+      test('Removing methods should remove the correct element from the list', () => {
+        let value = list.removeFirst();
+        expect(value).toBe(0);
+        expect(list.size()).toBe(5);
+        expect(list.toString()).toBe('1 2 5 9 10');
+
+        value = list.removeLast();
+        expect(value).toBe(10);
+        expect(list.size()).toBe(4);
+        expect(list.toString()).toBe('1 2 5 9');
+
+        expect(() => list.removeAt(-1)).toThrow('Index out of range');
+        expect(() => list.removeAt(4)).toThrow('Index out of range');
+
+        // remove at the first index
+        value = list.removeAt(0);
+        expect(value).toBe(1);
+        expect(list.size()).toBe(3);
+        expect(list.toString()).toBe('2 5 9');
+
+        // add 1 back
+        list.addFirst(1);
+        expect(list.size()).toBe(4);
+        expect(list.toString()).toBe('1 2 5 9');
+
+        // remove at the last index
+        value = list.removeAt(3);
+        expect(value).toBe(9);
+        expect(list.size()).toBe(3);
+        expect(list.toString()).toBe('1 2 5');
+
+        // add 9 back
+        list.addLast(9);
+        expect(list.size()).toBe(4);
+        expect(list.toString()).toBe('1 2 5 9');
+
+        // remove at an index in the middle
+        value = list.removeAt(1);
+        expect(value).toBe(2);
+        expect(list.size()).toBe(3);
+        expect(list.toString()).toBe('1 5 9');
+
+        // test removeLast and removeFirst when there's only 1 element in the list
+        list.removeLast();
+        list.removeFirst();
+        expect(list.size()).toBe(1);
+        expect(list.toString()).toBe('5');
+        value = list.removeFirst();
+        expect(value).toBe(5);
+        expect(list.size()).toBe(0);
+        expect(list.isEmpty()).toBeTruthy();
+
+        list.add(1);
+        value = list.removeLast();
+        expect(value).toBe(1);
+        expect(list.size()).toBe(0);
+        expect(list.isEmpty()).toBeTruthy();
+      });
+
+      test('Calling removeFirstNodeWithValue and removeAllNodesWithValue returns true if the node(s) found and removed, or false if not found', () => {
+        // Initial values 0 1 2 5 9 10
+        let result = list.removeFirstNodeWithValue(22);
+        expect(result).toBeFalsy();
+        expect(list.size()).toBe(6);
+
+        result = list.removeFirstNodeWithValue(0);
+        expect(result).toBeTruthy();
+        expect(list.size()).toBe(5);
+        expect(list.toString()).toBe('1 2 5 9 10');
+
+        result = list.removeFirstNodeWithValue(2);
+        expect(result).toBeTruthy();
+        expect(list.size()).toBe(4);
+        expect(list.toString()).toBe('1 5 9 10');
+
+        // Add duplicates values
+        list.insert(1, 2);
+        list.addFirst(9);
+        list.addLast(9);
+        expect(list.toString()).toBe('9 1 5 1 9 10 9');
+
+        result = list.removeFirstNodeWithValue(1);
+        expect(result).toBeTruthy();
+        expect(list.size()).toBe(6);
+        expect(list.toString()).toBe('9 5 1 9 10 9');
+
+        result = list.removeAllNodesWithValue(2);
+        expect(result).toBeFalsy();
+        expect(list.size()).toBe(6);
+        expect(list.toString()).toBe('9 5 1 9 10 9');
+
+        result = list.removeAllNodesWithValue(1);
+        expect(result).toBeTruthy();
+        expect(list.size()).toBe(5);
+        expect(list.toString()).toBe('9 5 9 10 9');
+
+        result = list.removeAllNodesWithValue(9);
+        expect(result).toBeTruthy();
+        expect(list.size()).toBe(2);
+        expect(list.toString()).toBe('5 10');
+      });
+
+      test('Peaking first/last should return the appropriate value', () => {
+        expect(list.peakFirst()).toBe(0);
+        expect(list.peakLast()).toBe(10);
+
+        list.add(11);
+        expect(list.toString()).toBe('0 1 2 5 9 10 11');
+        expect(list.peakLast()).toBe(11);
+        list.addFirst(-1);
+        expect(list.toString()).toBe('-1 0 1 2 5 9 10 11');
+        expect(list.peakFirst()).toBe(-1);
+
+        list.removeFirst();
+        expect(list.toString()).toBe('0 1 2 5 9 10 11');
+        expect(list.peakFirst()).toBe(0);
+
+        list.removeLast();
+        expect(list.toString()).toBe('0 1 2 5 9 10');
+        expect(list.peakLast()).toBe(10);
+      });
+
+      test('Clearing the list should remove all elements from the list', () => {
+        list.clear();
+        expect(list.isEmpty()).toBeTruthy();
+        expect(list.size()).toBe(0);
+      });
+
+      test('IndexOf method should return the correct index of the value in the list, or -1 if it does not exist', () => {
+        expect(list.indexOf(3)).toBe(-1);
+        expect(list.indexOf(1)).toBe(1);
+        expect(list.indexOf(9)).toBe(4);
+      });
+
+      test('Contains method should return true if the value exist, otherwise false', () => {
+        expect(list.contains(3)).toBeFalsy();
+        expect(list.contains(10)).toBeTruthy();
+      });
     });
   });
-});
+};
+
+linkedListTest(DoublyLinkedList, 'DoublyLinkedList');
+linkedListTest(SinglyLinkedList, 'SinglyLinkedList');
--- src/datastructures/linkedList.js
+++ src/datastructures/linkedList.js
@@ -98,7 +98,7 @@ export function DoublyLinkedList() {
   };
 
   /**
-   * Add a new node with the provided data to the beginning of the list, if the value hasn't existed in the list. O(1)
+   * Add a new node with the provided data to the beginning of the list. O(1)
    * @param {string | number} data
    */
   this.addFirst = data => {
@@ -112,7 +112,7 @@ export function DoublyLinkedList() {
   };
 
   /**
-   * Add a new node with the provided data to the end of the list, if the value hasn't existed in the list. O(1)
+   * Add a new node with the provided data to the end of the list. O(1)
    * @param {string | number} data
    */
   this.addLast = data => {
@@ -126,13 +126,13 @@ export function DoublyLinkedList() {
   };
 
   /**
-   * Add a new node with the provided data to the end of the list, if the value hasn't existed in the list. O(1)
+   * Add a new node with the provided data to the end of the list. O(1)
    * @param {string | number} data
    */
   this.add = data => this.addLast(data);
 
   /**
-   * Insert a new node to the list at the specified index, if the value hasn't existed and the index is within range. O(n)
+   * Insert a new node to the list at the specified index, if the index is within range. O(n)
    * @param {string | number} data
    * @param {number} index
    */
@@ -209,10 +209,11 @@ export function DoublyLinkedList() {
    * @returns {string | number} the removed node's data
    */
   this.removeAt = index => {
+    validateIndex(index);
+
     if (index === 0) return this.removeFirst();
     if (index === length - 1) return this.removeLast();
 
-    validateIndex(index);
     let trav = head;
     for (let i = 0; i < index; i++) trav = trav.next;
 
@@ -220,18 +221,50 @@ export function DoublyLinkedList() {
   };
 
   /**
-   * Remove a node with the specified value. O(n)
+   * Remove the first node with the specified value. O(n)
    * @param {string | number} value value of a node to be removed
-   * @returns {string | number} the removed node's data
+   * @returns {boolean} true if the node with that value is removed, false if there's no node with that value
    */
-  this.removeNodeWithValue = value => {
+  this.removeFirstNodeWithValue = value => {
+    if (this.isEmpty()) return false;
+
+    if (head.data === value) {
+      this.removeFirst();
+      return true;
+    }
+
     for (let trav = head; trav; trav = trav.next)
-      if (trav.data === value) return this.removeNode(trav);
-    return value;
+      if (trav.data === value) {
+        this.removeNode(trav);
+        return true;
+      }
+
+    return false;
   };
 
   /**
-   * Find the index of the node with the specified value. O(n)
+   * Remove the all nodes with the specified value. O(n)
+   * @param {string | number} value value of a node to be removed
+   * @returns {boolean} true if nodes with that value are removed, false if there's no node with that value
+   */
+  this.removeAllNodesWithValue = value => {
+    if (this.isEmpty()) return false;
+
+    let found = false;
+    for (let trav = head; trav; trav = trav.next)
+      if (trav.data === value) {
+        const prev = trav.prev;
+        this.removeNode(trav);
+        if (prev) trav = prev;
+
+        found = true;
+      }
+
+    return found;
+  };
+
+  /**
+   * Find the first index of the node with the specified value. O(n)
    * @param {string | number} value value of a node to be found
    * @returns {number} the node's index, or -1 if it doesn't exist
    */
@@ -261,6 +294,269 @@ export function DoublyLinkedList() {
  * Define the toString method for the list, it returns the list of data in each node, seperated by one space. O(n)
  */
 DoublyLinkedList.prototype.toString = function () {
+  const values = [];
+  for (let node of this[Symbol.iterator]()) values.push(node.data);
+  return values.join(' ');
+};
+
+/**
+ * Singly Linked List, accept number or string, and allow duplicate values
+ */
+export function SinglyLinkedList() {
+  /**
+   * a Node in a linked list that contains data and a pointer to the next node
+   * @param {string | number} data
+   * @param {Node} next a pointer to the next node
+   */
+  function Node(data, next = null) {
+    this.data = data;
+    this.next = next;
+  }
+
+  let length = 0;
+  let head = null;
+  let tail = null;
+
+  this.size = () => length;
+  this.isEmpty = () => length === 0;
+
+  /**
+   * Check if the index is within the valid range [0, length). O(1)
+   * @param {number} index an index number for the list
+   */
+  const validateIndex = index => {
+    if (index < 0 || index >= length) throw Error('Index out of range');
+  };
+
+  /**
+   * Return data at the list's head, if the list is not empty. O(1)
+   */
+  this.peakFirst = () => (head ? head.data : null);
+
+  /**
+   * Return data at the list's tail, if the list is not empty. O(1)
+   */
+  this.peakLast = () => (tail ? tail.data : null);
+
+  /**
+   * Clear the list by removing all nodes's pointers. O(n)
+   */
+  this.clear = () => {
+    if (this.isEmpty()) return;
+
+    let trav = head;
+    while (trav) {
+      const next = trav.next;
+      trav.next = trav.data = null;
+      trav = next;
+    }
+
+    head = tail = null;
+    length = 0;
+  };
+
+  /**
+   * Add a new node with the provided data to the beginning of the list. O(1)
+   * @param {string | number} data
+   */
+  this.addFirst = data => {
+    if (this.isEmpty()) head = tail = new Node(data);
+    else {
+      const node = new Node(data, head);
+      head = node;
+    }
+
+    length++;
+  };
+
+  /**
+   * Add a new node with the provided data to the end of the list. O(1)
+   * @param {string | number} data
+   */
+  this.addLast = data => {
+    const node = new Node(data);
+    if (this.isEmpty()) head = tail = node;
+    else {
+      tail.next = node;
+      tail = node;
+    }
+
+    length++;
+  };
+
+  /**
+   * Add a new node with the provided data to the end of the list. O(1)
+   * @param {string | number} data
+   */
+  this.add = data => this.addLast(data);
+
+  /**
+   * Insert a new node to the list at the specified index, if the index is within range. O(n)
+   * @param {string | number} data
+   * @param {number} index
+   */
+  this.insert = (data, index) => {
+    if (index === 0) return this.addFirst(data);
+    if (index === length) return this.addLast(data);
+
+    validateIndex(index);
+
+    let trav = head;
+    for (let i = 0; i < index - 1; i++) trav = trav.next; // trav pos will be at index - 1
+
+    const node = new Node(data, trav.next);
+    trav.next = node;
+
+    length++;
+  };
+
+  /**
+   * Remove the first node from the list. O(1)
+   * @returns {string | number} data the removed node's data
+   */
+  this.removeFirst = () => {
+    if (this.isEmpty()) return;
+
+    const prevHead = head;
+    const data = prevHead.data;
+    head = head.next;
+    prevHead.next = null;
+
+    length--;
+    if (this.isEmpty()) tail = null;
+
+    return data;
+  };
+
+  /**
+   * Remove the next node from the list from this specific node. O(n)
+   * @param {Node} node node before the node to be removed
+   * @returns {Node} the current node
+   */
+  this.removeNextNode = node => {
+    if (!node || !node.next) return node;
+
+    const next = node.next;
+    [node.next, next.next] = [next.next, null];
+    length--;
+
+    return node;
+  };
+
+  /**
+   * Get the node at index - 1
+   * @param {number} index the index of the node where we want to get the previous node from
+   */
+  this.getPrevNode = index => {
+    let trav = head;
+    for (let i = 0; i < index - 1; i++) trav = trav.next;
+    return trav;
+  };
+
+  /**
+   * Remove the last node from the list. O(n)
+   * @returns {string | number} data the removed node's data
+   */
+  this.removeLast = () => {
+    if (this.isEmpty()) return;
+    if (length === 1) return this.removeFirst();
+
+    let prevNode = this.getPrevNode(length - 1);
+    prevNode = this.removeNextNode(prevNode);
+    const data = tail.data;
+    tail = prevNode;
+    return data;
+  };
+
+  /**
+   * Remove a node at the specified index. O(n)
+   * @param {number} index the index of the node to be removed
+   * @returns {string | number} the removed node's data
+   */
+  this.removeAt = index => {
+    validateIndex(index);
+
+    if (index === 0) return this.removeFirst();
+    if (index === length - 1) return this.removeLast();
+
+    let prevNode = this.getPrevNode(index);
+    const data = prevNode.next.data;
+    this.removeNextNode(prevNode);
+
+    return data;
+  };
+
+  /**
+   * Remove node(s) with the specified value, if exists
+   * @param {string | number} value
+   * @param {boolean} single whether to delete only a single first value or not
+   */
+  const removeNodesWithValueHelper = (value, single = false) => {
+    if (this.isEmpty() || (length === 1 && head.data !== value)) return false;
+
+    if (head.data === value) {
+      this.removeFirst();
+      if (single) return true;
+    }
+
+    let found = false;
+    let trav = head;
+    while (trav && trav.next) {
+      if (trav.next.data === value) {
+        const node = this.removeNextNode(trav);
+        if (!node.next) tail = node;
+        found = true;
+        if (single) break;
+      }
+      trav = trav.next;
+    }
+
+    return found;
+  };
+
+  /**
+   * Remove the first node with the specified value. O(n)
+   * @param {string | number} value value of a node to be removed
+   * @returns {boolean} true if the node with that value is removed, false if there's no node with that value
+   */
+  this.removeFirstNodeWithValue = value =>
+    removeNodesWithValueHelper(value, true);
+
+  /**
+   * Remove the all nodes with the specified value. O(n)
+   * @param {string | number} value value of a node to be removed
+   * @returns {boolean} true if nodes with that value are removed, false if there's no node with that value
+   */
+  this.removeAllNodesWithValue = value => removeNodesWithValueHelper(value);
+
+  /**
+   * Find the first index of the node with the specified value. O(n)
+   * @param {string | number} value value of a node to be found
+   * @returns {number} the node's index, or -1 if it doesn't exist
+   */
+  this.indexOf = value => {
+    let trav = head;
+    for (let i = 0; trav, i < length; trav = trav.next, i++)
+      if (trav.data === value) return i;
+    return -1;
+  };
+
+  /**
+   * Check whether a value exists in the list. O(n)
+   * @param {string | number} value node's data
+   * @returns {boolean} true if it exists
+   */
+  this.contains = value => this.indexOf(value) !== -1;
+
+  /**
+   * Define a iterator for the list so that we can use it as an iterator. For each iteration, the iterator returns a node.
+   */
+  this[Symbol.iterator] = function* () {
+    for (let trav = head; trav; trav = trav.next) yield trav;
+  };
+}
+
+SinglyLinkedList.prototype.toString = function () {
   const values = [];
   for (let node of this[Symbol.iterator]()) values.push(node.data);
   return values.join(' ');
