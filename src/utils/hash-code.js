2b9c847dd1f101f9def5ffa3bf97958c424172bd
Kiera Nguyen <ngannguyen9017@gmail.com>
2020-10-08 11:46:02 -0700

    Include a hash function for strings

---
+++ src/utils/hash-code.js
@@ -0,0 +1,35 @@
+/**
+ * A a simple but high quality 53-bit hash. It's quite fast, provides very good hash distribution,
+ * and has significantly lower collision rates compared to any 32-bit hash. Similar to Murmur hash,
+ * it uses a combination of multiplication and Xorshift to generate the hash, but not as thorough.
+ *
+ * Note about the big magic mumbers:
+ *  - According to bryc, they are borrowed from other algorithms (L'Ecuyer, Knuth, MurmurHas)
+ *  - They were found through probabilistic testing/research
+ *  - They help increase the quality of the hash result
+ *
+ * bryc's answer can be found at {@link https://stackoverflow.com/a/52171480}
+ *
+ * @param {string} str a string value to be hashed
+ * @param {number} seed a variable to alternate streams of the same input
+ */
+export const hashCode = (str, seed = 0) => {
+  let h1 = 0xdeadbeef ^ seed;
+  let h2 = 0x41c6ce57 ^ seed;
+
+  for (let i = 0, ch; i < str.length; i++) {
+    ch = str.charCodeAt(i);
+    h1 = Math.imul(h1 ^ ch, 2654435761);
+    h2 = Math.imul(h2 ^ ch, 1597334677);
+  }
+
+  h1 =
+    Math.imul(h1 ^ (h1 >>> 16), 2246822507) ^
+    Math.imul(h2 ^ (h2 >>> 13), 3266489909);
+
+  h2 =
+    Math.imul(h2 ^ (h2 >>> 16), 2246822507) ^
+    Math.imul(h1 ^ (h1 >>> 13), 3266489909);
+
+  return 4294967296 * (2097151 & h2) + (h1 >>> 0);
+};
