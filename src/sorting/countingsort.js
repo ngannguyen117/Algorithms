087ab730c966e603060eb06a3abbaa3e4be596e3
Kiera Nguyen <ngannguyen9017@gmail.com>
2020-10-20 19:05:43 -0700

    Update counting sort to be stable

--- src/sorting/countingsort.js
+++ src/sorting/countingsort.js
@@ -1,15 +1,14 @@
-import { swap } from '../utils/swap';
-
 /**
- * Counting sort implementation. This implementation is inplace and not a stable.
+ * Counting sort implementation. This implementation is not inplace and is stable.
  * 
  * This sorting algorithm sorts element in an array by tracking the array's values frequency with the values as indexes.
  * 
  * Counting sort works best if the range between maxValue and minValue of the array is very small.
  * 
  * Time complexity: O(n + k) where n is the array length and k is the range between maxValue and minValue
+ * Space comlexity: n + k
  */
-export const countingSort = array => {
+export const countingSort = (array) => {
   if (!array) return;
 
   // find max and min values of the array
@@ -20,19 +19,22 @@ export const countingSort = array => {
     if (array[i] > maxValue) maxValue = array[i];
   }
 
-  if (maxValue === minValue) return;
+  if (maxValue === minValue) return array;
 
   const size = maxValue - minValue + 1;
-  const tempArr = [];
+  const frequency = [];
+  for (let i = 0; i < size; i++) frequency.push(0);
 
   // Track the frequency of each element
-  for (let i = 0; i < array.length; i++) {
-    const ind = array[i] - minValue;
-    if (!tempArr[ind]) tempArr[ind] = 0;
-    tempArr[ind]++;
-  }
+  for (let v of array) frequency[v - minValue]++;
+
+  // find the start index for each element
+  for (let i = 1; i < size; i++) frequency[i] += frequency[i - 1];
 
   // place each element in the correct position
-  for (let i = 0, k = 0; i < size; i++)
-    while (tempArr[i]-- > 0) array[k++] = i + minValue;
+  const sortedArray = [];
+  for (let v of array)
+    sortedArray[frequency[v - minValue]++ - 1] = v;
+
+  return sortedArray;
 };
