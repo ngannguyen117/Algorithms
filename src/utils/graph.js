fac9762c9fcf3c0cfcd88ab09afd68c18e8bd68a
Kiera Nguyen <ngannguyen9017@gmail.com>
2020-10-22 14:48:44 -0700

    Include method to add undirected edges

--- src/utils/graph.js
+++ src/utils/graph.js
@@ -12,7 +12,7 @@ export function Edge (from, to, cost) {
 }
 
 /**
- * Add a new edge to the graph
+ * Add a new directed edge to the graph
  * @param {Map<number, Edge[]>} graph Adjaciency list representation of a graph
  * @param {number} from the start vertex index of this edge
  * @param {number} to the end vertex index of this edge
@@ -26,4 +26,16 @@ export const addDirectedEdge = (graph, from, to, cost) => {
   }
 
   list.push(new Edge(from , to, cost));
+};
+
+/**
+ * Add a new undirected edge to the graph
+ * @param {Map<number, Edge[]>} graph Adjaciency list representation of a graph
+ * @param {number} from one end of this edge
+ * @param {number} to the other end of this edge
+ * @param {number} cost the weight/cost of going from one vertex to another
+ */
+export const addUndirectedEdge = (graph, from, to, cost = 0) => {
+  addDirectedEdge(graph, from, to, cost);
+  addDirectedEdge(graph, to, from, cost);
 };
