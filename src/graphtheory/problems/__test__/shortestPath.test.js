6522929107c6ffb2f1a915836fb5e30db163f2d0
Kiera Nguyen <ngannguyen9017@gmail.com>
2020-10-27 11:47:44 -0700

    Include test for shortest DAGs path with top sort

--- src/graphtheory/problems/__test__/shortestPath.test.js
+++ src/graphtheory/problems/__test__/shortestPath.test.js
@@ -1,4 +1,4 @@
-import { SSSPTopSort } from '../shortestPath';
+import { SSSPTopSort, shortestPathTopSort } from '../shortestPath';
 import { addDirectedEdge } from '../../../utils/graph';
 
 describe('Test all shortest path algorithms', () => {
@@ -41,5 +41,48 @@ describe('Test all shortest path algorithms', () => {
     expect(distance[2]).toBe(0);
     expect(distance[3]).toBe(1);
     expect(distance[4]).toBe(6);
+  });
+
+  test('Find the shortest path on a DAG from a start node to an end node by using top sort', () => {
+    //           (0)          (6)
+    //         /  |  \
+    //      3 /  2|   \ 3
+    //       /    |    \
+    //      <  6 \|/    >
+    //    (1)--->(2)    (5)
+    //     |     /|     /
+    //   1 |   1/ |10  /7
+    //     |   /  |   /
+    //     | /    | /
+    //     <   5  <
+    //    (3)--->(4)
+
+    const graph = new Map();
+    const numVertices = 7;
+    addDirectedEdge(graph, 0, 1, 3);
+    addDirectedEdge(graph, 0, 2, 2);
+    addDirectedEdge(graph, 0, 5, 3);
+    addDirectedEdge(graph, 1, 3, 1);
+    addDirectedEdge(graph, 1, 2, 6);
+    addDirectedEdge(graph, 2, 3, 1);
+    addDirectedEdge(graph, 2, 4, 10);
+    addDirectedEdge(graph, 3, 4, 5);
+    addDirectedEdge(graph, 5, 4, 7);
+
+    let result = shortestPathTopSort(graph, numVertices, 0, 4);
+    expect(result.distance).toBe(8);
+    expect(result.path).toStrictEqual([0, 2, 3, 4]);
+
+    result = shortestPathTopSort(graph, numVertices, 2, 4);
+    expect(result.distance).toBe(6);
+    expect(result.path).toStrictEqual([2, 3, 4]);
+
+    result = shortestPathTopSort(graph, numVertices, 0, 0);
+    expect(result.distance).toBe(0);
+    expect(result.path).toStrictEqual([0]);
+
+    result = shortestPathTopSort(graph, numVertices, 6, 0);
+    expect(result.distance).toBeNull();
+    expect(result.path).toStrictEqual([]);
   });
 });
