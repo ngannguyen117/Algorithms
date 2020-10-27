2e4b4f3f0f800075e5bd2eff084ba2b99254c2a3
Kiera Nguyen <ngannguyen9017@gmail.com>
2020-10-27 11:47:14 -0700

    Implement finding shortest path for DAGs using top sort

--- src/graphtheory/problems/shortestPath.js
+++ src/graphtheory/problems/shortestPath.js
@@ -18,9 +18,10 @@ import { Edge } from '../../utils/graph';
  * to compute all the shortest distances from the start node.
  * 
  * Complexity O(V + E)
+ * 
  * @param {Map<number, Edge[]>} graph an adjaciency representation of a DAG 
  * @param {number} numVertices number of vertices of the graph 
- * @param {number} start the starting index
+ * @param {number} start the starting's node index
  * @returns {number[]} an array of indexes holding the distances from the start node to all other nodes
  */
 export const SSSPTopSort = (graph, numVertices, start) => {
@@ -38,4 +39,48 @@ export const SSSPTopSort = (graph, numVertices, start) => {
   }
 
   return dist;
+};
+
+/**
+ * Find the shortest path from a starting node to an ending node on a DAG using top sort.
+ * This implementation terminates early when it reaches the end node.
+ * 
+ * Complexity O(V + E)
+ * 
+ * @param {Map<number, Edge[]>} graph an adjaciency representation of a DAG 
+ * @param {number} numVertices number of vertices of the graph 
+ * @param {number} start the starting node's index
+ * @param {number} end the ending node's index
+ * @returns {{distance: number; path: number[]}} distance from start to end and an array of
+ * indexes showing the shortest path from start node to end node
+ */
+export const shortestPathTopSort = (graph, numVertices, start, end) => {
+  const topologicalOrdering = topSort(graph, numVertices);
+  const prev = [];
+  const dist = [];
+  dist[start] = 0;
+
+  let index = 0;
+  let id = topologicalOrdering[index];
+  while (id !== end) {
+    if (dist[id] != null) {
+      const edges = graph.get(id);
+      if (edges) for (let edge of edges) {
+        const newDist = dist[id] + edge.cost;
+        if (!dist[edge.to] || newDist < dist[edge.to]) {
+          dist[edge.to] = newDist;
+          prev[edge.to] = id;
+        }
+      }
+    }
+
+    id = topologicalOrdering[++index];
+  }
+
+  const path = [];
+  for (let at = end; at != null; at = prev[at]) path.push(at);
+  path.reverse();
+
+  if (path[0] !== start) return { distance: null, path: [] };
+  return { distance: dist[end], path };
 };
