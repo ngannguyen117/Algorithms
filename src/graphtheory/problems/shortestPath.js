c03d689f20f9e70bcb40a597252b48b16b23c190
Kiera Nguyen <ngannguyen9017@gmail.com>
2020-10-28 09:40:53 -0700

    Implement SSSP and shortest Path from start to end using Dijkstras

--- src/graphtheory/problems/shortestPath.js
+++ src/graphtheory/problems/shortestPath.js
@@ -8,6 +8,7 @@
 
 import { topSort } from '../fundamental/topologicalSort';
 import { Edge } from '../../utils/graph';
+import { IndexedDHeap } from '../../datastructures/priorityqueue';
 
 /**
  * Single Source Shortest Path (SSSP) finds the shortest distances
@@ -77,6 +78,131 @@ export const shortestPathTopSort = (graph, numVertices, start, end) => {
     id = topologicalOrdering[++index];
   }
 
+  const path = [];
+  for (let at = end; at != null; at = prev[at]) path.push(at);
+  path.reverse();
+
+  if (path[0] !== start) return { distance: null, path: [] };
+  return { distance: dist[end], path };
+};
+
+/**
+ * Single Source Shortest Path with Dijkstra's algorithm.
+ * 
+ * Find the shortest distances from a starting node to all nodes on
+ * NON-NEGATIVE edge weight graphs using Dijkstra's algorithm (cycles allowed).
+ * The non-negative edge weight constraint is imposed to ensure that
+ * once a node has been visited, its optimal distance cannot be improved.
+ * 
+ * This property is important because it enables Dijkstra's algorithm to
+ * act in a greedy manner by always selecting the next most promising node.
+ * 
+ * In a lazy implementation:
+ *  - Use a priority queue to keep track of the next most promising node
+ *  - Store stale (index, distance) pairs => waste space
+ *  - Optimization: skip this iteration if the current best distance is better than the one we just receive from the PQ.
+ *  - Complexity O(E x log(V))
+ * 
+ * In an eager implementation:
+ *  - Use an Indexed Priority Queue to efficiently update the (index, distance) pairs with better distance found
+ *  - In addition, if we use a D-ary heap, it'll speed up update operations (deletes are a little bit slower). Best D = E/V
+ *  - Complexity O(E x log base D of (V))
+ *  - Much better for dense graphs which have a lot of decrease key operations.
+ * 
+ * @param {Map<number, Edge[]>} graph an adjaciency representation of a non-negative edge weight graph
+ * @param {number} numVertices number of vertices of the graph 
+ * @param {number} start the starting node's index
+ * @returns {number[]} an array of indexes holding the distances from the start node to all other nodes
+ */
+export const SSSPDijkstras = (graph, numVertices, start) => {
+  let edgeCount = 0;
+  for (let edges of graph.values()) edgeCount += edges.length;
+
+  // Keep an Indexed Priority Queue (ipq) of the next most promising node to visit
+  const degree = Math.floor(edgeCount / numVertices);
+  const ipq = new IndexedDHeap(degree, numVertices);
+  ipq.insert(start, 0);
+
+  // Maintain an array of the minimum distance to each node.
+  const dist = [...Array(numVertices)].fill(Number.POSITIVE_INFINITY);
+  dist[start] = 0;
+
+  const visited = [...Array(numVertices)].fill(false);
+
+  while (!ipq.isEmpty()) {
+    const nodeId = ipq.peakKeyIndex();
+
+    visited[nodeId] = true;
+    const value = ipq.pollValue();
+
+    // We already found a better path before we got to process this node so we can ignore it
+    if (value > dist[nodeId]) continue;
+
+    const edges = graph.get(nodeId);
+    if (edges) for (let edge of edges) {
+      if (visited[edge.to]) continue;
+
+      // Relax edge by updating minimum cost if applicable.
+      const newDist = dist[nodeId] + edge.cost;
+      if (newDist < dist[edge.to]) {
+        dist[edge.to] = newDist;
+
+        if (ipq.contains(edge.to)) ipq.decrease(edge.to, newDist);
+        else ipq.insert(edge.to, newDist);
+      }
+    }
+  }
+
+  return dist;
+};
+
+/**
+ * Find the shortest path from a starting node to an ending node on
+ * NON-NEGATIVE edge weight graphs using Dijkstra's algorithm.
+ * This implementation terminates early when it reaches the end node.
+ * 
+ * @param {Map<number, Edge[]>} graph an adjaciency representation of a non-negative edge weight graph
+ * @param {number} numVertices number of vertices of the graph 
+ * @param {number} start the starting node's index
+ * @param {number} end the ending node's index
+ * @returns {{distance: number; path: number[]}} distance from start to end and an array of
+ * indexes showing the shortest path from start node to end node
+ */
+export const shortestPathDijkstras = (graph, numVertices, start, end) => {
+  let edgeCount = 0;
+  for (let edges of graph.values()) edgeCount += edges.length;
+
+  const degree = Math.floor(edgeCount / numVertices);
+  const ipq = new IndexedDHeap(degree, numVertices);
+  ipq.insert(start, 0);
+
+  const dist = [...Array(numVertices)].fill(Number.POSITIVE_INFINITY);
+  const visited = [...Array(numVertices)].fill(false);
+  const prev = [];
+  dist[start] = 0;
+
+  while (!ipq.isEmpty()) {
+    if (nodeId === end) break;
+
+    const nodeId = ipq.peakKeyIndex();
+    visited[nodeId] = true;
+    const value = ipq.pollValue();
+    if (value > dist[nodeId]) continue;
+
+    const edges = graph.get(nodeId);
+    if (edges) for (let edge of edges) {
+      const newDist = dist[nodeId] + edge.cost;
+      if (newDist < dist[edge.to]) {
+        dist[edge.to] = newDist;
+        prev[edge.to] = nodeId;
+
+        if (ipq.contains(edge.to)) ipq.decrease(edge.to, newDist);
+        else ipq.insert(edge.to, newDist);
+      }
+    }
+  }
+
+  // reconstruct path
   const path = [];
   for (let at = end; at != null; at = prev[at]) path.push(at);
   path.reverse();
