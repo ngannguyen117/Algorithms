c7a22e702bbb105026b86a168185a5690f2d0591
Kiera Nguyen <ngannguyen9017@gmail.com>
2020-10-28 17:34:15 -0700

    Include test for SCCs

--- src/graphtheory/problems/__test__/connectedComponents.test.js
+++ src/graphtheory/problems/__test__/connectedComponents.test.js
@@ -1,8 +1,8 @@
-import { addUndirectedEdge } from '../../../utils/graph';
-import { findCCsDFS, findCCsUnionFind } from '../connectedComponents';
+import { addUndirectedEdge, addDirectedEdge } from '../../../utils/graph';
+import { findCCsDFS, findCCsUnionFind, findSCCsTarjans } from '../connectedComponents';
 
 const testConnectedComponents = (findCCs, technique) => {
-  let graph = new Map();
+  const graph = new Map();
   let size = 7;
 
   // Setup a graph with four connected components
@@ -29,3 +29,33 @@ const testConnectedComponents = (findCCs, technique) => {
 
 testConnectedComponents(findCCsDFS, 'Depth First Search');
 testConnectedComponents(findCCsUnionFind, 'UnionFind');
+
+test('Test Finding Strongly Connected Components SCCs using Tarjan', () => {
+  const graph = new Map();
+  const numVertices = 8;
+  addDirectedEdge(graph, 6, 0);
+  addDirectedEdge(graph, 6, 2);
+  addDirectedEdge(graph, 3, 4);
+  addDirectedEdge(graph, 6, 4);
+  addDirectedEdge(graph, 2, 0);
+  addDirectedEdge(graph, 0, 1);
+  addDirectedEdge(graph, 4, 5);
+  addDirectedEdge(graph, 5, 6);
+  addDirectedEdge(graph, 3, 7);
+  addDirectedEdge(graph, 7, 5);
+  addDirectedEdge(graph, 1, 2);
+  addDirectedEdge(graph, 7, 3);
+  addDirectedEdge(graph, 5, 0);
+
+  // There are 3 components: [0, 1, 2], [3, 7], [4, 5, 6]
+  const { components, count } = findSCCsTarjans(graph, numVertices);
+  expect(count).toBe(3);
+  expect(components[0]).toBe(components[1]);
+  expect(components[0]).toBe(components[2]);
+  expect(components[3]).toBe(components[7]);
+  expect(components[4]).toBe(components[5]);
+  expect(components[4]).toBe(components[6]);
+  expect(components[0]).not.toBe(components[7]);
+  expect(components[0]).not.toBe(components[5]);
+  expect(components[7]).not.toBe(components[4]);
+});
