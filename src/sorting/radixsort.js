9c238b607e352f67f10437709f20d5530cc52d27
Kiera Nguyen <ngannguyen9017@gmail.com>
2020-11-02 15:42:31 -0800

    Update Radix sort

--- src/sorting/radixsort.js
+++ src/sorting/radixsort.js
@@ -1,43 +1,35 @@
-const getNumberOfDigits = number => {
-  let count = 0;
-  if (count >= 0) count++;
-  for (let i = number / 10; i >= 1; i /= 10) count++;
+const getNumberOfDigits = num => {
+  let count = 1;
+  for (num /= 10; num >= 1; num /= 10) count++;
   return count;
 };
 
 /**
  * Radix Sort implementation.
  * 
- * O(nd) where n is the array length and d is the max number of digit an element has
+ * O(nd) where n is the array length and d is max element's the number of digit
  */
 export const radixSort = array => {
   if (!array) return;
 
   const countingSort = placeValue => {
+    const index = i => Math.floor(array[i] / placeValue) % range;
     const range = 10;
-    const frequency = [];
-    for (let i = 0; i < range; i++) frequency.push(0);
+    const frequency = Array(range).fill(0);
 
-    for (let v of array) {
-      const ind = Math.floor(v / placeValue) % range;
-      frequency[ind]++;
-    }
-
+    for (let i = 0; i < range; i++) frequency[index(i)]++;
     for (let i = 1; i < range; i++) frequency[i] += frequency[i - 1];
 
     const sortedArray = [];
-    for (let i = array.length - 1; i >= 0; i--) {
-      const digit = Math.floor(array[i] / placeValue) % range;
-      sortedArray[frequency[digit]-- - 1] = array[i];
-    }
+    for (let i = array.length - 1; i >= 0; i--)
+      sortedArray[frequency[index(i)]-- - 1] = array[i];
 
     array = sortedArray;
   };
 
-  const maximum = Math.max.apply(null, array);
-  let numberOfDigits = getNumberOfDigits(maximum);
+  let numDigits = getNumberOfDigits(Math.max.apply(null, array));
   let placeValue = 1;
-  while (numberOfDigits-- > 0) {
+  while (numDigits-- > 0) {
     countingSort(placeValue);
     placeValue *= 10;
   }
