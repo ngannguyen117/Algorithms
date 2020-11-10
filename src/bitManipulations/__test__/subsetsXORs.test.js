e6e4be53b2ebacae63f8b1aee7133920b051cc67
Kiera Nguyen <ngannguyen9017@gmail.com>
2020-11-10 10:24:42 -0800

    Test operations on subsets XORs

---
+++ src/bitManipulations/__test__/subsetsXORs.test.js
@@ -0,0 +1,15 @@
+import { computeXorOfSubsetsXors, computeSumOfSubsetsXors } from '../subsetsXORs';
+
+describe('Test operations on subsets XORs', () => {
+  test('Test computing XOR of all subsets XORs', () => {
+    expect(computeXorOfSubsetsXors([1, 2, 3])).toBe(0);
+    expect(computeXorOfSubsetsXors([3])).toBe(3);
+    expect(computeXorOfSubsetsXors([])).toBe(0);
+    expect(computeXorOfSubsetsXors([5, 7])).toBe(0);
+    expect(computeXorOfSubsetsXors([10])).toBe(10);
+  });
+
+  test('Test computing Sum of all subsets XORs', () => {
+    expect(computeSumOfSubsetsXors([1, 5, 6])).toBe(28);
+  });
+});
