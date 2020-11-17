bfb2607b57bfa5b0cc6be75e72e449a6b558e9d5
Kiera Nguyen <ngannguyen9017@gmail.com>
2020-11-17 15:28:18 -0800

    Implement pattern searching using KMP

--- src/strings/patternSearching.js
+++ src/strings/patternSearching.js
@@ -6,31 +6,32 @@ import { SuffixArray } from '../datastructures/suffixarray';
  * Should only use this algorithm if the text is short.
  * This algorithm returns the start index of only 1 occurence.
  * 
- * Time complexity: O(nlogn) for SA construction & O(mlogn) for individual
- * query (m is pattern's length, n is text's length)
+ * Time complexity: O(nlogn) SA construction & O(mlogn) individual query
  * 
  * @param {string} text A string 
  * @param {string} pattern pattern/substring
  */
 export const searchPatternSA = (text, pattern) => {
-  if (pattern == null) return -1;
+  if (text == null || pattern == null) return -1;
   if (pattern === '') return 0;
 
+  const n = text.length;
+  const m = pattern.length;
+  if (m > n) return -1;
+
   const sa = new SuffixArray(text).getSuffixArray();
-  const size = text.length;
-  const substrLen = pattern.length;
   let start = 0;
-  let end = size - 1;
+  let end = n - 1;
   let suffixStr = '';
 
   // Do binary search
   while (start <= end) {
     const mid = start + ((end - start) >>> 1);
     const suffixIndex = sa[mid];
-    const suffixLen = size - suffixIndex;
+    const suffixLen = n - suffixIndex;
 
     // Extract part of the suffix we need to compare
-    const len = suffixLen < substrLen ? suffixLen : substrLen;
+    const len = suffixLen < m ? suffixLen : m;
     suffixStr = text.slice(suffixIndex, suffixIndex + len);
 
     if (suffixStr === pattern) return suffixIndex;
@@ -39,4 +40,61 @@ export const searchPatternSA = (text, pattern) => {
   }
 
   return -1;
+};
+
+/**
+ * Find all occurences' start index of matching pattern in the text (including 
+ * overlapping matching) using KMP (Knuth-Morris-Pratt algorithm).
+ * Should use this algorithm if the text string is very large.
+ * 
+ * KMP explanation {@link https://www.youtube.com/watch?v=GTJr8OvyEVQ}
+ * 
+ * Time complexity: O(n)
+ * Space complexity: O(m)
+ * 
+ * @param {string} text A string 
+ * @param {string} pattern pattern/substring
+ */
+export const searchPatternKMP = (text, pattern) => {
+  /**
+   * For each index i, compute the longest match between the prefix
+   * starting at 0 and the suffix starting at i
+   */
+  const computeLongestMatch = () => {
+    const arr = Array(m).fill(0);
+
+    for (let i = 1, len = 0; i < m;)
+      if (pattern[i] === pattern[len]) arr[i++] = ++len;
+      else if (len > 0) len = arr[len - 1];
+      else i++;
+
+    return arr;
+  };
+
+  if (text == null || pattern == null) return [];
+  if (pattern === '') return [0];
+
+  const n = text.length;
+  const m = pattern.length;
+  if (m > n) return [];
+
+  const matches = [];
+  const longestMatch = computeLongestMatch();
+  let i = 0;
+  let j = 0;
+
+  while (i < n) {
+    if (text[i] === pattern[j]) {
+      i++;
+      j++;
+    } else if (j === 0) i++;
+    else j = longestMatch[j - 1];
+
+    if (j === m) {
+      matches.push(i - m);
+      j = longestMatch[j - 1];
+    }
+  }
+
+  return matches;
 };
